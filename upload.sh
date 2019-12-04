8f198142ac42ce5c78dd9eb09d7e84b03e1eb5e6
liuchong <liu.chong@immomo.com>
2019-12-04 12:13:13 +0800

    修改：打包脚本配置

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cur_dir=`pwd`
 version='1.0.0-'$(date "+%Y%m%d.%H%M")
-sed -i '' "s/VERSION=.*/VERSION=${version}/g" ${cur_dir}/justicecenter/gradle.properties
-./gradlew build justice:bintrayUpload -PbintrayUser=cosmossaas@gmail.com -PbintrayKey=fb7469a974c53aa31ed28bc25a0c26b415657e4f -PdryRun=false
-echo ${version}'上传成功'
+#sed -i '' "s/VERSION=.*/VERSION=${version}/g" ${cur_dir}/justicecenter/gradle.properties
+#./gradlew build justice:bintrayUpload -PbintrayUser=cosmossaas@gmail.com -PbintrayKey=fb7469a974c53aa31ed28bc25a0c26b415657e4f -PdryRun=false
+#echo ${version}'上传成功'
+sed -i '' "s/justiceVersion =.*/justiceVersion =\"${version}\"/g" ${cur_dir}/build.gradle
